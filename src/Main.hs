cf06f25e65d80ffc2f57405aed739e1a258ff91f
Daniel Choi <dhchoi@gmail.com>
2013-12-16 15:08:43 -0500

    Test nested form

--- src/Main.hs
+++ src/Main.hs
@@ -12,12 +12,12 @@ import Text.Blaze ((!))
 import qualified Data.Text as T
 
 import qualified Text.Blaze.Html5 as H
--- import qualified Text.Blaze.Html5.Attributes as A
+import qualified Text.Blaze.Html5.Attributes as A
 
 import Text.Digestive
 import Text.Digestive.Blaze.Html5
 import Text.Digestive.Snap
-import Text.Digestive.Util
+import Text.Digestive.Util (readMaybe)
 import Snap.Blaze
 
 data User = User
@@ -25,13 +25,41 @@ data User = User
   , userMail :: Text
   } deriving (Show)
 
+type Version = [Int]
+
+data Category = Web | Text | Math deriving (Bounded, Enum, Eq, Show)
+
+data Package = Package Text Version Category deriving (Show)
+
+data Release = Release User Package deriving (Show)
+
 userForm :: Monad m => Form Text m User
 userForm = User 
-  <$> "name" .: check "Can't be empty" (not . T.null) (text  Nothing)
+  <$> "name" .: check "Can't be empty" checkTextNotNull (text  Nothing)
   <*> "mail" .: check "Not a valid email address" checkEmail (text Nothing)
+
+packageForm :: Monad m => Form Text m Package
+packageForm = Package
+    <$> "name" .: text Nothing
+    <*> "version" .: validate validateVersion (text (Just "0.0.0.1"))
+    <*> "category" .: choice categories Nothing
+  where 
+    categories = [(x, T.pack (show x)) | x <- [minBound .. maxBound]]
+
+releaseForm :: Monad m => Form Text m Release
+releaseForm = Release 
+    <$> "author" .: userForm
+    <*> "package" .: packageForm
 
 checkEmail :: Text -> Bool
 checkEmail = isJust . T.find (== '@')
+
+checkTextNotNull :: Text -> Bool
+checkTextNotNull = not . T.null
+
+validateVersion :: Text -> Result Text Version
+validateVersion = maybe (Error "Cannot parse version") Success .
+  mapM (readMaybe . T.unpack) . T.split (== '.')
 
 userView :: View H.Html -> H.Html
 userView view = do
@@ -44,6 +72,26 @@ userView view = do
   inputText "mail" view
   H.br
 
+releaseView :: View H.Html -> H.Html
+releaseView view = do
+    H.h2 "Author"
+    userView $ subView "author" view
+
+    H.h2 "Package"
+    childErrorList "package" view
+
+    label "package.name" view "Name: "
+    inputText "package.name" view
+    H.br
+
+    label "package.version" view "Version: "
+    inputText "package.version" view
+    H.br
+    label "package.category" view "Category: "
+    inputSelect "package.category" view
+    H.br 
+
+
 main :: IO ()
 main = quickHttpServe site
 
@@ -55,20 +103,30 @@ template :: H.Html -> H.Html
 template body = H.docTypeHtml $ do
   H.head $ do
     H.title "snap digestive functors blaze test"
+    css
   H.body body
+
+css :: H.Html
+css = H.style ! A.type_ "text/css" $ do
+   "label {width: 130px; float: left; clear: both}"
+   "ul.digestive-functors-error-list {"
+   "    color: red;"
+   "    list-style-type: none;"
+   "    padding-left: 0px;"
+   "}"
 
 formHandler :: Snap ()
 formHandler = do
-    r <- runForm "userform" userForm
+    r <- runForm "releaseForm" releaseForm
     case r of 
       (view, Nothing) -> do 
         liftIO $ putStrLn "rendering form"
         let view' = fmap H.toHtml view
         blaze $ template $ 
           form view' "/" $ do
-            userView view'
+            releaseView view'
             H.br
             inputSubmit "Submit"
-      (_, Just user) -> 
-        writeText $ T.pack $ show user
+      (_, Just release) -> 
+        writeText $ T.pack $ show release
 
